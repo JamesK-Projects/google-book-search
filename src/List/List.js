c2086575fc6c2d4373c4661dad543a3e75b38fbf
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-10-05 15:02:11 -0400

    initial upload

--- src/List/List.js
+++ src/List/List.js
@@ -1,1 +1,20 @@
+import React, { Component } from 'react';
+import ListItem from '../ListItem/ListItem'
 
+class List extends Component {
+    render() { 
+        console.log(this.props.books.items)
+        return(
+            Object.values(this.props.books.items).map((book, index) => {
+                
+                return (
+                        <div>
+                            <ListItem book={book} key={index}/>
+                        </div>
+                );
+            })   
+        )
+    }
+}
+ 
+export default List;
